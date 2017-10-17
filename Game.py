20525dba33b004f4ddff499db629eac3b70c6a0b
Weeya Weerawatpruchya <weeyaa.w@gmail.com>
2017-10-17 09:47:00 +0700

    bomb+sweet

--- Game.py
+++ Game.py
@@ -1,5 +1,4 @@
 import arcade
-import math
 from random import randint
 from modelGame import World
 SCREEN_WIDTH = 500
@@ -7,9 +6,9 @@ SCREEN_HEIGHT = 620
 Scaling = 0.4
 class Candy(arcade.Sprite):
     def reset_pos(self):
-        self.center_y = randint(SCREEN_HEIGHT,1000)
-        self.center_x = randint(10,SCREEN_WIDTH-10)
-        #self.center_y = randint(SCREEN_HEIGHT + 10,SCREEN_HEIGHT + 100)
+        self.center_y = randint(SCREEN_HEIGHT,1200)
+        self.center_x = randint(5,SCREEN_WIDTH-5)
+        #print(self.center_x)
     
     def update(self):
         
@@ -17,6 +16,26 @@ class Candy(arcade.Sprite):
         if self.top < 0:
             self.reset_pos()  
 
+class Sweet(arcade.Sprite):
+    def reset_pos(self):
+        self.center_y = randint(SCREEN_HEIGHT,2000)
+        self.center_x = randint(5,SCREEN_WIDTH-5)
+    
+    def update(self):
+        self.center_y -= 4  #speed of sweet
+        if self.top < 0:
+            self.reset_pos()  
+
+class Bomb(arcade.Sprite):
+    def reset_pos(self):
+        self.center_y = randint(SCREEN_HEIGHT,1000)
+        self.center_x = randint(5,SCREEN_WIDTH-5)
+
+    def update(self):
+        self.center_y -= 3  #speed of bomb
+        if self.top < 0:
+            self.reset_pos()
+'''
 class Snow:
     def __init__(self):
         self.x = 0
@@ -25,7 +44,8 @@ class Snow:
     def reset_pos(self):
         self.y = randint(SCREEN_HEIGHT, SCREEN_HEIGHT+100)
         self.x = randint(0,SCREEN_WIDTH)
-        
+''' 
+
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model', None)
@@ -44,27 +64,50 @@ class GameWindow(arcade.Window):
     def __init__(self,width,height):
         super().__init__(width, height)
  
-        arcade.set_background_color(arcade.color.BLACK)
+        self.background = None 
+        self.background = arcade.load_texture('image/bg2.jpg')
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-        self.wizard = ModelSprite('image/w3.png',model=self.world.wizard)
-
+        self.imagewiz = ['image/w3.png','image/left.png','image/right.png']
+        self.wizard = ModelSprite(self.imagewiz[0],model=self.world.wizard)
+        
         self.all_sprites_list = None
         self.candy_list = None
-        self.snow_list = None
+        self.sweet_list = None
+        self.bomb_list = None
+        #self.snow_list = None
     
+        self.score = 0
+        
     def candy_falling(self):
         self.all_sprites_list = arcade.SpriteList()
         self.candy_list = arcade.SpriteList()
+        self.sweet_list = arcade.SpriteList()
+        self.bomb_list = arcade.SpriteList()
 
-        for i in range(15):
+        for i in range(5):
             candy = Candy('image/candy.png',Scaling)
             candy.center_x = randint(10,SCREEN_WIDTH-10)
-            candy.center_y = randint(150,SCREEN_HEIGHT)
-    
+            candy.center_y = randint(1,SCREEN_HEIGHT)
+
             self.all_sprites_list.append(candy)
             self.candy_list.append(candy)
+        
+        for i in range(1):
+            sweet = Sweet('image/sweet.png',0.5)
+            sweet.center_x = randint(10,SCREEN_WIDTH-10)
+            sweet.center_y = randint(600,SCREEN_HEIGHT)
+    
+            self.all_sprites_list.append(sweet)
+            self.sweet_list.append(sweet)
 
+        for i in range(1):
+            bomb = Bomb('image/bomb.png',0.05)
+            #bomb.center_x = randint(10,SCREEN_WIDTH-10)
+            #bomb.center_y = randint(0,10)
+    
+            self.all_sprites_list.append(bomb)
+            self.bomb_list.append(bomb)
+    '''
     def start_falling(self):
         self.snow_list = []
 
@@ -77,32 +120,69 @@ class GameWindow(arcade.Window):
             snow.speed = randint(20,40)
 
             self.snow_list.append(snow)
-
+    '''
     def update(self, delta):
         self.world.update(delta)
+        self.wizard = ModelSprite(self.imagewiz[self.world.status],model=self.world.wizard)
         self.all_sprites_list.update()
+        '''
+        if self.wizard.hit(self.candy,10):
+            self.candy.random_location()
+            self.score += 1
+        '''
+        if((Candy.center_x==self.world.wizard.x) and (Candy.center_y==self.world.wizard.y)):
+            print(Candy.center_x)
+        hit_list_candy = arcade.check_for_collision_with_list(self.wizard,self.candy_list)
+        hit_list_sweet = arcade.check_for_collision_with_list(self.wizard,self.sweet_list)
+        hit_list_bomb = arcade.check_for_collision_with_list(self.wizard,self.bomb_list)
+
+        for candy in hit_list_candy:
+            candy.reset_pos()
+            self.score += 1
+    
+        for sweet in hit_list_sweet:
+            sweet.reset_pos()
+            self.score += 2
+        
+        for bomb in hit_list_bomb:
+            bomb.reset_pos()
+            print("in")
+            if(self.score>=10):
+                self.score -= 10
+            elif(self.score-10<=0):
+                self.score = 0
+
+        '''
         for snow in self.snow_list:
             snow.y -= snow.speed * delta
 
             if snow.y < 0:
                 snow.reset_pos()
-
+        '''
+        
     def on_draw(self):
+        
         arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.wizard.draw() 
+        
         self.all_sprites_list.draw()
+
+        '''
         for snow in self.snow_list:
             arcade.draw_circle_filled(snow.x, snow.y,snow.size, arcade.color.WHITE)
-        
-        #output = "Score: {}".format(self.score)
-        #arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+        '''
+        output = "Score: {}".format(self.score)
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 16)
         
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
+    def on_key_release(self, key, key_modifiers):
+        self.world.on_key_release(key, key_modifiers)
 
 def main():
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
-    window.start_falling()
+    #window.start_falling()
     window.candy_falling()
     arcade.set_window(window)
     arcade.run()
--- modelGame.py
+++ modelGame.py
@@ -1,66 +1,46 @@
 import arcade.key
-D_UP = 1
-D_RIGHT = 2
-D_DOWN = 3
-D_LEFT = 4
- 
-DIR_OFFSET = { D_UP: (0,1),
-               D_RIGHT: (1,0),
-               D_DOWN: (0,-1),
-               D_LEFT: (-1,0) }
-'''
-class Model:
-    def __init__(self, world, x, y, angle):
-        self.world = world
-        self.x = x
-        self.y = y
-        self.angle = 0
-'''
+SPEED = 5
+
 class Wizard:
-    WAIT = 0
-    step = 6
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
 
-        self.wait_time = 0
-
-        self.direction = D_RIGHT 
-    def update(self, delta):
-        self.wait_time += delta
-
-        if self.wait_time < Wizard.WAIT:
-            return
-        
-        self.x = (self.x + 5*DIR_OFFSET[self.direction][0])%self.world.width 
-       
-        '''
-        if self.x >= self.world.width:
-            self.x = 0
-        elif self.x <= 0:
-            self.x = width
-        self.x += self.step
-        '''
-        self.wait_time = 0
-
-
+        self.delta_x = 0
+    
+    def move(self):
+        self.x += self.delta_x
+        if self.x < 0:
+            self.x = self.world.width
+        elif self.x > self.world.width:
+            self.x = 0       
+    
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-
-        self.wizard = Wizard(self, 250, 130)
-        #self.candy = Candy(self, 250, 300)
-
+        self.status = 0
+        self.wizard = Wizard(self, 250, 100)
+        
+    
     def update(self, delta):
-        self.wizard.update(delta)
-
+        self.wizard.move()
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.RIGHT:
-            self.wizard.direction = D_RIGHT
+            self.status = 2
+            self.wizard.delta_x = SPEED
+            
         elif key == arcade.key.LEFT:
-            self.wizard.direction = D_LEFT
-        elif key == arcade.key.UP:
-            self.wizard.direction = D_UP #break!!
-            #self.snake.direction = D_DOWN
+            self.status = 1
+            self.wizard.delta_x = -SPEED
+    
+    def on_key_release(self, key, key_modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.status = 0
+            self.wizard.delta_x = 0
+            
+    
+      
+            
+        
